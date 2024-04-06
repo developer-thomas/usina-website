2a4a31e96d8950c74517351177e9dce2b04d1202
developer-thomas <andepipous@gmail.com>
2024-04-06 18:48:26 -0400

    features: create an mouseevent at the image, wich the image moves according to the mouse. Adjusted the container width at mobile

--- src/app/components/pages/about/about.component.ts
+++ src/app/components/pages/about/about.component.ts
@@ -5,4 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss', './responsive.scss'],
 })
-export class AboutComponent {}
+export class AboutComponent {
+  translateX: number = 0;
+  translateY: number = 0;
+
+  moveImage(event: MouseEvent) {
+    // getBoundingClientRect retorna o tamanho do elemento e a posição viewport dele
+    const containerRect = (event.target as HTMLElement).getBoundingClientRect();
+    const offsetX = event.clientX - containerRect.left;
+    const offsetY = event.clientY - containerRect.top;
+
+    const maxTranslate = 50;
+    const percentX = (offsetX / containerRect.width - 0.5) * maxTranslate;
+    const percentY = (offsetY / containerRect.height - 0.5) * maxTranslate;
+
+    this.translateX = percentX;
+    this.translateY = percentY;
+  }
+
+  resetImage() {
+    this.translateX = 0;
+    this.translateY = 0;
+  }
+}
