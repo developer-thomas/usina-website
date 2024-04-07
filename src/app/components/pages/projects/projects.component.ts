c1944690f0b641c01898a9fa325b060456f716bc
developer-thomas <andepipous@gmail.com>
2024-04-07 19:09:10 -0400

    features: add assets to projects page

--- src/app/components/pages/projects/projects.component.ts
+++ src/app/components/pages/projects/projects.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from 'src/app/services/projects.service';
+import { Projects } from 'src/models/Projects';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss', './responsive.scss'],
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  public projects: Projects[] = [];
+  public visibleProjects: Projects[] = [];
 
+  imagensPorPagina = 6;
+  paginaAtual = 0;
+
+  constructor(private projectsService: ProjectsService) {}
+
+  ngOnInit(): void {
+    this.getImagens();
+  }
+
+  getImagens(): void {
+    this.projectsService
+      .getAllProjects()
+      .subscribe((res) => (this.projects = res));
+    this.carregarImagensVisiveis();
+  }
+
+  carregarImagensVisiveis(): void {
+    const startIndex = this.paginaAtual;
+    const endIndex = startIndex + this.imagensPorPagina;
+    this.visibleProjects = this.projects.slice(startIndex, endIndex);
+  }
+  public showMore(): void {
+    this.imagensPorPagina += 3;
+    this.carregarImagensVisiveis();
+  }
 }
