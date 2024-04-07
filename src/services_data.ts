ba72db18012b14ee9cde6070e9e360eb06ff97f3
developer-thomas <andepipous@gmail.com>
2024-04-07 02:27:46 -0400

    features: create services data and its type as a class

---
+++ src/services_data.ts
@@ -0,0 +1,50 @@
+import { ServicesType } from './models/ServicesType';
+export const sample_services: ServicesType[] = [
+  {
+    id: 0,
+    icon: './assets/services-icons/totem-icon.png',
+    title: 'Totens',
+    description:
+      'Totens personalizados para sua empresa feitos com materiais de qualidade',
+    tag: 'totens',
+  },
+  {
+    id: 1,
+    icon: './assets/services-icons/acm-icon.png',
+    title: 'Revestimento ACM/ACP',
+    description: 'Revestimentos de ACM/ACP personalizados para sua empresa ',
+    tag: 'revestimento-acm-acp',
+  },
+  {
+    id: 2,
+    icon: './assets/services-icons/banner-icon.png',
+    title: 'Fachadas ACM/ACP',
+    description:
+      'Fachadas personalizados para sua empresa feitos com materiais de qualidade',
+    tag: 'fachadas-acm-acp',
+  },
+  {
+    id: 3,
+    icon: './assets/services-icons/fachada-externa-icon.png',
+    title: 'Letreiros Externos',
+    description:
+      'Letreiros Externos personalizados para sua empresa feitos com materiais de qualidade',
+    tag: 'letreiros-externos',
+  },
+  {
+    id: 4,
+    icon: './assets/services-icons/neon-icon.png',
+    title: 'Letras em Neon',
+    description:
+      'Letras em Neon personalizados para sua empresa feitos com materiais de qualidade',
+    tag: 'letras-neon',
+  },
+  {
+    id: 5,
+    icon: './assets/services-icons/banner.png',
+    title: 'Letreiros Internos PVC',
+    description:
+      'Letreiros Internos PVC personalizados para sua empresa feitos com materiais de qualidade',
+    tag: 'letreiro-interno-pvc',
+  },
+];
