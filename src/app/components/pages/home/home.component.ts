7436f2a855e611d5bcc77170931b52154f7d8bee
developer-thomas <andepipous@gmail.com>
2024-04-09 17:02:00 -0400

    features: add video background to home component and change the text to logo

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  styleUrls: ['./home.component.scss', './responsive.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('videoElement')
+  videoElementRef!: ElementRef;
+  videoElement!: HTMLVideoElement;
+
+  ngAfterViewInit(): void {
+    this.videoElement = this.videoElementRef.nativeElement;
+  }
+
+  playBackground(): void {
+    if (this.videoElement.paused) {
+      this.videoElement.play();
+    }
+  }
+}
