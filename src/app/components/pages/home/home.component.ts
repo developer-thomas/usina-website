a4b167e15846a68206a99602e599655024b0e737
developer-thomas <andepipous@gmail.com>
2024-06-02 14:31:17 -0400

    features: set background effect as fog and change the header position (is bugging at mobile)

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,22 +1,35 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+
+declare var VANTA: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss', './responsive.scss'],
 })
-export class HomeComponent implements AfterViewInit {
-  @ViewChild('videoElement')
-  videoElementRef!: ElementRef;
-  videoElement!: HTMLVideoElement;
-
-  ngAfterViewInit(): void {
-    this.videoElement = this.videoElementRef.nativeElement;
-  }
-
-  playBackground(): void {
-    if (this.videoElement.paused) {
-      this.videoElement.play();
-    }
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+    VANTA.FOG({
+      el: '#home-section',
+      mouseControls: false,
+      touchControls: false,
+      gyroControls: false,
+      minHeight: 100,
+      minWidth: 200,
+      highlightColor: 0x4a4e69,
+      midtoneColor: 0x14213d,
+      lowlightColor: 0x081c15,
+      baseColor: 0x262a13,
+      blurFactor: 0.48,
+      speed: 0.8,
+      zoom: 0.7,
+      alpha: 0.1,
+    });
   }
 }
