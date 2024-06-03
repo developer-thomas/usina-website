015c72afacaf6f00db8b8dd34e2b6643e3ca58bf
developer-thomas <andepipous@gmail.com>
2024-06-03 00:21:51 -0400

    features: added a new toggle icon and fixed its functionality

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss', './responsive.component.scss'],
 })
 export class HeaderComponent {
+  public isCollapsed: boolean = false;
 
+  public toggle() {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
