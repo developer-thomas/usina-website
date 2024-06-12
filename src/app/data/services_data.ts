c9bde900ecd73651377fbb674734b35a2067e57b
developer-thomas <andepipous@gmail.com>
2024-06-12 09:28:13 -0400

    update services_data informations

---
+++ src/app/data/services_data.ts
@@ -0,0 +1,52 @@
+import { ServicesType } from '../../models/ServicesType';
+
+export const sample_services: ServicesType[] = [
+  {
+    id: 0,
+    icon: './assets/services-icons/totem-icon.png',
+    title: 'Totens',
+    description:
+      'Totens personalizados com design exclusivo, ideais para destacar a sua marca.',
+    tag: 'totens',
+  },
+  {
+    id: 1,
+    icon: './assets/services-icons/acm-icon.png',
+    title: 'Revestimento ACM/ACP',
+    description:
+      'Revestimentos em ACM/ACP com acabamento premium, perfeitos para fachadas modernas e resistentes.',
+    tag: 'revestimento-acm-acp',
+  },
+  {
+    id: 2,
+    icon: './assets/services-icons/banner-icon.png',
+    title: 'Fachadas ACM/ACP',
+    description:
+      'Fachadas em ACM/ACP projetadas para oferecer durabilidade e estética, valorizando a imagem da sua empresa.',
+    tag: 'fachadas-acm-acp',
+  },
+  {
+    id: 3,
+    icon: './assets/services-icons/fachada-externa-icon.png',
+    title: 'Letreiros Externos',
+    description:
+      'Letreiros externos robustos e personalizados, ideais para reforçar a identidade visual do seu negócio.',
+    tag: 'letreiros-externos',
+  },
+  {
+    id: 4,
+    icon: './assets/services-icons/neon-icon.png',
+    title: 'Letras em Neon',
+    description:
+      'Alta qualidade, perfeitas para criar uma atmosfera vibrante e atrativa para o seu estabelecimento.',
+    tag: 'letras-neon',
+  },
+  {
+    id: 5,
+    icon: './assets/services-icons/banner.png',
+    title: 'Letreiros Internos',
+    description:
+      'Personalizados para proporcionar um visual elegante e profissional dentro do seu ambiente.',
+    tag: 'letreiro-interno-pvc',
+  },
+];
