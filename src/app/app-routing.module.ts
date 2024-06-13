c6d5031590f524528c7d406990b5d67571dbe16d
developer-thomas <andepipous@gmail.com>
2024-06-13 14:19:44 -0400

    refactor: create the main route to root-page component and 'servicos/:tag' path to render the work-detail component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WorkDetailComponent } from './components/pages/work-detail/work-detail.component';
+import { RootPageComponent } from './components/pages/root-page/root-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'servicos/:tag',
+    component: WorkDetailComponent,
+  },
+  {
+    path: '',
+    component: RootPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
