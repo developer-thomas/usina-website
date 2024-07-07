cbccabc00d1944a7d45a8fc92c037e89f62178ef
developer-thomas <andepipous@gmail.com>
2024-07-06 21:11:41 -0400

    fix: fix aos library bug when user got back to root route using the browser back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component } from '@angular/core';
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'usina-website';
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    AOS.init();
+  }
 }
